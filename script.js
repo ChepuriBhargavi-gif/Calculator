ea4496622ab8039e2f7518c8237e3eaf5e971157
Bhargavi <165979711+ChepuriBhargavi-gif@users.noreply.github.com>
2026-07-14 21:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ function addToDisplay(input){
 
 function calculate(){
 
+    try{
+
     display.value = eval(display.value);
+
+    } catch(error) {
+
+        display.value = "Error";
+        
+    }
 
 }
 
